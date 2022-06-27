7d80e4307550afea151bc77d649c65daad2b3a88
Anton <a.volynskiy@brl.ru>
2022-06-28 00:34:49 +1000

    commit

--- controllers/products.js
+++ controllers/products.js
@@ -21,6 +21,9 @@ const getAllProducts = async (req, res) => {
     if (sort === 'new') {
         result = result.sort({'_id': -1})
     }
+    if (sort === 'old') {
+        result = result.sort({'_id': 1})
+    }
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 15;
     const skip = (page - 1) * limit;
